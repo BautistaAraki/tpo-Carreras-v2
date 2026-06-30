3a821013ae9e322a6b134428ad08fe5947cfaf74
BautistaAraki <Bauti.araki@gmail.com>
2026-06-30 19:24:16 -0300

    Update JugadorDTO.java

--- src/Dto/JugadorDTO.java
+++ src/Dto/JugadorDTO.java
@@ -1,37 +1,6 @@
 package Dto;
 
-public class JugadorDTO {
-	private String nombre;
-	private String mail;
-	private int puntaje;
-	 public JugadorDTO(String nombre, String mail, int puntaje) {
-	        this.nombre = nombre;
-	        this.mail = mail;
-	        this.puntaje = puntaje;
-	    }
-
-	    public String getNombre() {
-	        return nombre;
-	    }
-
-	    public void setNombre(String nombre) {
-	        this.nombre = nombre;
-	    }
-
-	    public String getMail() {
-	        return mail;
-	    }
-
-	    public void setMail(String mail) {
-	        this.mail = mail;
-	    }
-
-	    public int getPuntaje() {
-	        return puntaje;
-	    }
-
-	    public void setPuntaje(int puntaje) {
-	        this.puntaje = puntaje;
-	    }
-
+public record JugadorDTO(String nombre, String mail, int puntaje, String caballoSeleccionado) {
+    public JugadorDTO(String nombre, String mail) { this(nombre, mail, 0, null); }
 }
+
