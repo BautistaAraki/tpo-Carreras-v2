733ed58547e42661aae26acbbba71829079abd7a
BautistaAraki <Bauti.araki@gmail.com>
2026-06-30 19:21:55 -0300

    Update jugador.java

--- src/Modelo1/jugador.java
+++ src/Modelo1/jugador.java
@@ -1,33 +1,31 @@
 package Modelo1;
 
-public class jugador {
-	private String nombre;
-	private String mail;
-	private int puntajeActual;
-	private Caballo seleccionarCaballo;
-	private Boolean esIA;
-	public void seleccionarCaballo(Caballo caballo){
-		seleccionarCaballo=caballo;	
-	}
-	public jugador(String nombre, String mail) {
-	        this.nombre = nombre;
-	        this.mail = mail;
-	        this.puntajeActual = 0;
-	    }
-	public void sumarPuntos(int puntos) {
-	    puntajeActual = puntajeActual + puntos;
-	}
-	public Caballo getcaballoseleccionado() {
-		return seleccionarCaballo;
-	}
-	public String getnombre () {
-		return nombre;
-	}
-	public String getmail () {
-		return mail;
-	}
-	public int getPuntaje() {
-		return puntajeActual;
-	}
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "jugador")
+public class Jugador {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 80)
+    private String nombre;
+    @Column(nullable = false, unique = true, length = 120)
+    private String mail;
+    @Column(name = "puntaje", nullable = false)
+    private int puntajeActual;
+    @Transient
+    private Caballo caballoSeleccionado;
+
+    protected Jugador() {}
+    public Jugador(String nombre, String mail) { this.nombre = nombre; this.mail = mail; }
+    public void seleccionarCaballo(Caballo caballo) { caballoSeleccionado = caballo; }
+    public void sumarPuntos(int puntos) { puntajeActual += puntos; }
+    public Long getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getMail() { return mail; }
+    public int getPuntaje() { return puntajeActual; }
+    public Caballo getCaballoSeleccionado() { return caballoSeleccionado; }
+}
+
 
 }
