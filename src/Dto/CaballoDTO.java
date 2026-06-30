b2572acbd03fcf338887563fa27a78b0e0a929a5
BautistaAraki <Bauti.araki@gmail.com>
2026-06-30 19:23:09 -0300

    Update CaballoDTO.java

--- src/Dto/CaballoDTO.java
+++ src/Dto/CaballoDTO.java
@@ -1,53 +1,9 @@
 package Dto;
 
-public class CaballoDTO {
-	 private String nombre;
-	    private double velocidadBase;
-	    private double resistencia;
-	    private double energiaActual;
-	    private double distanciaRecorrida;
-	    private String perfil;
-
-	    public CaballoDTO() {
-	    }
-
-	    public CaballoDTO(String nombre,
-	                      double velocidadBase,
-	                      double resistencia,
-	                      double energiaActual,
-	                      double distanciaRecorrida,
-	                      String perfil) {
-
-	        this.nombre = nombre;
-	        this.velocidadBase = velocidadBase;
-	        this.resistencia = resistencia;
-	        this.energiaActual = energiaActual;
-	        this.distanciaRecorrida = distanciaRecorrida;
-	        this.perfil = perfil;
-	    }
-
-	    public String getNombre() {
-	        return nombre;
-	    }
-
-	    public double getVelocidadBase() {
-	        return velocidadBase;
-	    }
-
-	    public double getResistencia() {
-	        return resistencia;
-	    }
-
-	    public double getEnergiaActual() {
-	        return energiaActual;
-	    }
-
-	    public double getDistanciaRecorrida() {
-	        return distanciaRecorrida;
-	    }
-
-	    public String getPerfil() {
-	        return perfil;
-	    }
-
+public record CaballoDTO(String nombre, double velocidadBase, double resistencia,
+        double energiaActual, double distanciaRecorrida, String perfil, boolean seleccionado) {
+    public CaballoDTO(String nombre, double velocidadBase, double resistencia, String perfil) {
+        this(nombre, velocidadBase, resistencia, resistencia, 0, perfil, false);
+    }
 }
+
